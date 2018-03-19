4b8e28e5d463cf516630a8bc19008cbcc43703b9
dalia ramos and Kayla Ondracek <dalia@>
2018-03-19 10:14:36 -0700

    add tests and methods for tamagotchi food and happy levels going down, as well as ability to feed tamagotchi

--- spec/tama-spec.js
+++ spec/tama-spec.js
@@ -1,10 +1,13 @@
 import { Tama } from './../src/tama.js';
 
 describe('Tama', function() {
-  let newTama = new Tama("Frank");
+  let newTama;
 
   beforeEach(function() {
     jasmine.clock().install();
+    newTama = new Tama("Frank");
+    newTama.setFoodLevel();
+    newTama.setHappyLevel();
   });
 
   afterEach(function() {
@@ -15,6 +18,18 @@ describe('Tama', function() {
     expect(newTama.name).toEqual("Frank");
     expect(newTama.foodLevel).toEqual(10);
     expect(newTama.happyLevel).toEqual(10);
+  });
+
+  it('The food and happiness levels should go down 1 every 10 seconds you dont feed it', function() {
+    jasmine.clock().tick(10001);
+    expect(newTama.foodLevel).toEqual(9);
+    expect(newTama.happyLevel).toEqual(9);
+  });
+
+  it('Should increase food levels when fed', function() {
+    newTama.feed();
+    expect(newTama.foodLevel).toEqual(12);
+
   });
 
 });
--- src/tama.js
+++ src/tama.js
@@ -4,4 +4,20 @@ export class Tama {
     this.foodLevel = 10;
     this.happyLevel = 10;
   }
+  setFoodLevel(){
+    setInterval(() => {
+      this.foodLevel--;
+    }, 10000);
+  }
+  setHappyLevel(){
+    setInterval(() => {
+      this.happyLevel--;
+    }, 10000);
+  }
+
+  feed() {
+    this.foodLevel += 2;
+  }
+
+
 }
